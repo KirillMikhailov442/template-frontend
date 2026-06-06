3cc398c662fdfd1c437d81a7ef8e2719dbb3d3de
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2026-06-06 23:52:14 +0500

    feat: добавлена возможность быть не авторизованным на страницах дял dev режима

--- src/app/layouts/authorization.tsx
+++ src/app/layouts/authorization.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Outlet } from 'react-router';
 
+import { isDev } from '@shared/config/env';
+
 const AuthorizationLayout = () => {
   const token = localStorage.getItem('token');
 
-  if (!token) {
+  if (!token && !isDev) {
     return <Navigate to="/login" replace />;
   }
 
