b25f9160c2732fd9f6f41a0e145a5400bbbb02e1
Ivan Sychev <ivansycev0@gmail.com>
2026-06-07 01:48:02 +0500

    feat: add buttons on template card

--- src/pages/Templates/TemplateCard.tsx
+++ src/pages/Templates/TemplateCard.tsx
@@ -1,11 +1,22 @@
+import { IconButton } from '@chakra-ui/react';
+import { LucideEdit, LucideTrash } from 'lucide-react';
+
 type IProps = {
   imgSrc: string;
   title: string;
-}
+};
 
-const TemplateCard = ({imgSrc, title}: IProps) =>{
+const TemplateCard = ({ imgSrc, title }: IProps) => {
   return (
-    <div className=" min-w-[200px] max-w-[300px] rounded-2xl overflow-hidden flex flex-col shadow-md">
+    <div className=" min-w-[200px] max-w-[300px] rounded-2xl overflow-hidden flex flex-col shadow-md relative group">
+      <div className="absolute top-2 w-full opacity-0 group-hover:opacity-60 px-2 gap-2 flex justify-end transition">
+        <IconButton size={'xs'} rounded={'lg'}>
+          <LucideTrash />
+        </IconButton>
+        <IconButton size={'xs'} rounded={'lg'}>
+          <LucideEdit />
+        </IconButton>
+      </div>
       <div className="grow w-full">
         <img src={imgSrc} alt={title} className="w-full h-full object-cover" />
       </div>
@@ -14,6 +25,6 @@ const TemplateCard = ({imgSrc, title}: IProps) =>{
       </div>
     </div>
   );
-}
+};
 
 export default TemplateCard;
