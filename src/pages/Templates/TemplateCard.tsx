4e9a321bcefc342d84efbfa8734c176d81fc4d73
Ivan Sychev <ivansycev0@gmail.com>
2026-06-07 03:14:35 +0500

    feat: hide buttons in menu

--- src/pages/Templates/TemplateCard.tsx
+++ src/pages/Templates/TemplateCard.tsx
@@ -1,5 +1,5 @@
-import { IconButton } from '@chakra-ui/react';
-import { LucideEdit, LucideTrash } from 'lucide-react';
+import { IconButton, Menu, Portal } from '@chakra-ui/react';
+import { LucideEdit, LucideMoreVertical, LucideTrash } from 'lucide-react';
 
 type IProps = {
   imgSrc: string;
@@ -8,15 +8,31 @@ type IProps = {
 
 const TemplateCard = ({ imgSrc, title }: IProps) => {
   return (
-    <div className=" min-w-[200px] max-w-[300px] rounded-2xl overflow-hidden flex flex-col shadow-md relative group">
-      <div className="absolute top-2 w-full opacity-0 group-hover:opacity-60 px-2 gap-2 flex justify-end transition">
-        <IconButton size={'xs'} rounded={'lg'}>
-          <LucideTrash />
-        </IconButton>
-        <IconButton size={'xs'} rounded={'lg'}>
-          <LucideEdit />
-        </IconButton>
+    <div className="min-w-[200px] max-w-[300px] rounded-2xl overflow-hidden flex flex-col shadow-md relative group">
+      <div className="absolute top-2 right-2">
+        <Menu.Root>
+          <Menu.Trigger>
+            <IconButton rounded="xl" size="xs" className="opacity-50 md:opacity-0 group-hover:opacity-50">
+              <LucideMoreVertical />
+            </IconButton>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="edit">
+                  <LucideEdit />
+                  Редактировать
+                </Menu.Item>
+                <Menu.Item value="delete">
+                  <LucideTrash />
+                  Удалить
+                </Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
       </div>
+
       <div className="grow w-full">
         <img src={imgSrc} alt={title} className="w-full h-full object-cover" />
       </div>
