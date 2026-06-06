3c8d80bb1d4562112130c75ca27e8b858491eed7
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2026-06-06 20:06:50 +0500

    fix: починин файл дял pipeline

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,24 +1,5 @@
-import { useEffect, useState, type FC } from 'react';
-
-import { Button } from '@chakra-ui/react';
-
-import { testFetch } from './fetch';
+import type { FC } from 'react';
 
 export const TestPage: FC = () => {
-  const [status, setStatus] = useState<string>('Loading...');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await testFetch();
-        setStatus('Data fetched successfully');
-      } catch {
-        setStatus('Error fetching data');
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return <div className="from-blue-50 to-indigo-100 p-8"></div>;
 };
