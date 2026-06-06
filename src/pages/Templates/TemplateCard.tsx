edeb796599f93dd9b4cc5626d03c3a52e2a6ee08
Ivan Sychev <ivansycev0@gmail.com>
2026-06-07 00:41:14 +0500

    feat: add templates page

---
+++ src/pages/Templates/TemplateCard.tsx
@@ -0,0 +1,19 @@
+type IProps = {
+  imgSrc: string;
+  title: string;
+}
+
+const TemplateCard = ({imgSrc, title}: IProps) =>{
+  return (
+    <div className="h-[200px] w-[300px] rounded-2xl overflow-hidden flex flex-col">
+      <div className="grow w-full">
+        <img src={imgSrc} alt={title} className="w-full h-full object-cover" />
+      </div>
+      <div className="text-center border-t bg-[var(--color-white)]">
+        {title}
+      </div>
+    </div>
+  );
+}
+
+export default TemplateCard;
--- src/shared/constants/routes.consts.ts
+++ src/shared/constants/routes.consts.ts
@@ -3,4 +3,5 @@ export enum AppRoute {
   Templates = '/templates',
   Login = '/login',
   NotFound = '*',
+  Error = '/error',
 }
