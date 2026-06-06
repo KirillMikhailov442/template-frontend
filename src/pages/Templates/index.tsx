e5b8d6e8798d1fd60f615d2681d92449c31c81f3
Ivan Sychev <ivansycev0@gmail.com>
2026-06-07 00:41:14 +0500

    feat: add templates page

---
+++ src/pages/Templates/index.tsx
@@ -0,0 +1,34 @@
+import TemplateCard from './TemplateCard';
+
+const mockData = [
+  {
+    id: 1,
+    title: 'Шаблон 1',
+    imgSrc: '/template.webp',
+  },
+  {
+    id: 2,
+    title: 'Шаблон 2',
+    imgSrc: '/template.webp',
+  },
+  {
+    id: 3,
+    title: 'Шаблон 3',
+    imgSrc: '/template.webp',
+  },
+];
+
+const Templates = () => {
+  const data = mockData;
+
+  const renderTemplateCards = () => {
+    return mockData.map(item => {
+       return <TemplateCard key={item.id} {...item} />;
+    });
+  };
+  return <div className=" h-full grow p-5 flex gap-5">
+    {data && renderTemplateCards()}
+  </div>;
+};
+
+export default Templates;
--- src/shared/constants/routes.consts.ts
+++ src/shared/constants/routes.consts.ts
@@ -2,4 +2,5 @@ export enum AppRoute {
   Root = '/',
   Templates = '/templates',
   NotFound = '*',
+  Error = '/error',
 }
